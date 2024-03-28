19115e76601c9606a8080da6b317e0c61cf3be63
santhoshi <santhoshi6april@gmail.com>
2024-03-28 16:47:45 +0530

    funtions

--- 04-functions.sh
+++ 04-functions.sh
@@ -8,10 +8,10 @@ example
 example1() {
   echo value of 1 - $1
   echo value of 3 - $3
-  echo value of * - $*
-  echo value of # - $#
+  echo "value of * - $*"
+  echo "value of # - $#"
   echo value of 2 - $2
 }
 
 example1 10 20 30
-example $*
+example "$*"
