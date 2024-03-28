a26e549a27a9886ff9a1f9b745f7b46073b42e8d
santhoshi <santhoshi6april@gmail.com>
2024-03-28 16:44:09 +0530

    funtions

--- 04-functions.sh
+++ 04-functions.sh
@@ -7,7 +7,7 @@ example
 #function spl variables
 example1() {
   echo value of 1 - $1
-  echo value of 0 - $0
+  echo value of 3 - $3
   echo value of * - $*
   echo value of # - $#
   echo value of 2 - $2
