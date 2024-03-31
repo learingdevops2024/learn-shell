8b5f53f508470d76004ccd75a2c1322f6ee33409
santhoshi <santhoshi6april@gmail.com>
2024-03-31 10:19:07 +0530

    Colors

---
+++ 01-printing.sh
@@ -0,0 +1,18 @@
+echo hello world
+#colour printing
+# Red      -31
+#green     -32
+#Yellow    -33
+#Blue      -34
+#Magenta   -35
+#cyan      -36
+
+#Syantax : echo -e "\e[COLmMESSAGE\-e[0m"
+
+echo -e "\e[31mhello in red colour\e[0m"
+echo -e "\e[32mhello in blue colour\e[0m"
+echo -e "\e[33mhello in green colour\e[0m"
+echo -e "\e[34mhello in yellow colour\e[0m"
+echo -e "\e[35mhello in blue colour\e[0m"
+echo -e "\e[36mhello in magenta colour\e[0m"
+
